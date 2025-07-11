377ff9b2203ae36e0deab3115b181612c3a347a7
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-11 12:24:42 +0530

    Added route for creating task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,4 +15,16 @@ class TasksController < ApplicationController
       render status: :not_found, json: { error: I18n.t("task.not_found") }
     end
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    render_notice(I18n.t("task.created"))
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
