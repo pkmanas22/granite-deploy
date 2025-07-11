0b33a7a2dfee4d05747a4af616daf7e2f79a12b8
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-11 13:06:23 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,15 @@
-import '../stylesheets/application.scss';
+import "../stylesheets/application.scss";
+import ReactRailsUJS from "react_ujs";
+import App from "../src/App";
 
-import ReactRailsUJS from 'react_ujs';
-import App from '../src/App';
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
 
-import { setAuthHeaders } from 'apis/axios';
-import { initializeLogger } from 'common/logger';
-
+registerIntercepts();
 initializeLogger();
 setAuthHeaders();
 
 const componentsContext = { App };
-ReactRailsUJS.getConstructor = (name) => {
+ReactRailsUJS.getConstructor = name => {
   return componentsContext[name];
 };
--- config/build/config.js
+++ config/build/config.js
@@ -1,20 +1,22 @@
-import { createRequire } from 'module';
-import path from 'path';
+import { createRequire } from "module";
+import path from "path";
 
-import { absolutePath } from './constants.js';
+import { absolutePath } from "./constants.js";
 
 const require = createRequire(import.meta.url);
 
 const alias = {
-  images: path.resolve(process.cwd(), 'app/assets/images'),
-  crypto: require.resolve('crypto-browserify'),
-  path: require.resolve('path-browserify'),
-  buffer: require.resolve('buffer'),
-  stream: require.resolve('stream-browserify'),
-  apis: absolutePath('src/apis'),
-  common: absolutePath('src/common'),
-  components: absolutePath('src/components'),
-  assets: absolutePath('../assets'),
+  images: path.resolve(process.cwd(), "app/assets/images"),
+  crypto: require.resolve("crypto-browserify"),
+  path: require.resolve("path-browserify"),
+  buffer: require.resolve("buffer"),
+  stream: require.resolve("stream-browserify"),
+  apis: absolutePath("src/apis"),
+  common: absolutePath("src/common"),
+  components: absolutePath("src/components"),
+  constants: absolutePath("src/constants"),
+  utils: absolutePath("src/utils"),
+  assets: absolutePath("../assets"),
 };
 
 export { alias };
