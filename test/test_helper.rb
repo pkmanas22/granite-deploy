5c48dd133b177728a8b1df3324f84b0e9989837f
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-21 12:02:15 +0530

    Integrated factory-bot and faker

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,14 +20,11 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors) unless ENV["COVERAGE"]
+class ActiveSupport::TestCase
+  include FactoryBot::Syntax::Methods
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    # fixtures :all
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors) unless ENV["COVERAGE"]
 
-    # Add more helper methods to be used by all tests here...
-  end
+  # Add more helper methods to be used by all tests here...
 end
