f8c33f1e725cb832a761585fb652cd045135d53f
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-28 12:15:35 +0530

    Fixed order of items in Active Record models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  MAX_NAME_LENGTH = 255
+  MAX_NAME_LENGTH = 35
   MIN_PASSWORD_LENGTH = 6
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   MAX_EMAIL_LENGTH = 255
@@ -9,20 +9,23 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
-  has_secure_password
-  has_secure_token :authentication_token
 
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates :email, presence: true,
     uniqueness: { case_sensitive: false },
     length: { maximum: MAX_EMAIL_LENGTH },
     format: { with: VALID_EMAIL_REGEX }
-  validates :password, length: { minimum: MIN_PASSWORD_LENGTH }, if: -> { password.present? }
+  validates :password,
+    presence: true,
+    confirmation: true,
+    length: { minimum: MIN_PASSWORD_LENGTH }
   validates :password_confirmation, presence: true, on: :create
 
+  before_save :to_lowercase
   before_destroy :assign_tasks_to_task_owners
 
-  before_save :to_lowercase
+  has_secure_password
+  has_secure_token :authentication_token
 
   private
 
